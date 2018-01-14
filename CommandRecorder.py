f1b41f5e521f101421a7b8169cb5a59235100c3d
Jan Wilczek <janekw@vp.pl>
2018-01-14 21:28:50 +0100

    Hitting space or clicking the button starts the command recording and handling process.

---
+++ CommandRecorder.py
@@ -0,0 +1,42 @@
+import pyaudio
+import wave
+
+
+
+class CommandRecorder:
+
+    def __init__(self):
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 2
+        self.RATE = 44100
+        self.CHUNK = 1024
+        self.RECORD_SECONDS = 5
+
+    def startRecording(self, filename="command.wav"):
+        WAVE_OUTPUT_FILENAME = filename
+
+        audio = pyaudio.PyAudio()
+
+        # start Recording
+        stream = audio.open(format=self.FORMAT, channels=self.CHANNELS,
+                            rate=self.RATE, input=True,
+                            frames_per_buffer=self.CHUNK)
+        print("recording...")
+        frames = []
+
+        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+            data = stream.read(self.CHUNK)
+            frames.append(data)
+        print("finished recording")
+
+        # stop Recording
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        waveFile.setnchannels(self.CHANNELS)
+        waveFile.setsampwidth(audio.get_sample_size(self.FORMAT))
+        waveFile.setframerate(self.RATE)
+        waveFile.writeframes(b''.join(frames))
+        waveFile.close()
--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-import admin
-import listen_for_commands
-from CommandHandler import CommandHandler
+#!/usr/bin/python
 
-if __name__=="__main__":
-    if not admin.isUserAdmin():
-        admin.runAsAdmin()
-    listen_for_commands()
+#import admin
+from tkinter import *
+from tkinter import ttk
+from CommandRecorder import CommandRecorder
+#from CommandHandler import CommandHandler
+
+def record_and_handle_command():
+    command_recorder.startRecording("command" + str(listen_for_commands.counter) + ".wav")
     # Here comes handling the recording in 'command.wav'
     # (response/command, args) = ...
-    handler = CommandHandler()
+    # handler = CommandHandler()
     # handler.handle(command,args)
+
+def listen_for_commands(event):
+    if 'counter' not in listen_for_commands.__dict__:
+            listen_for_commands.counter = 0
+    state_label_text.set("Recording command...")
+    root.update_idletasks()
+    record_and_handle_command()
+    state_label_text.set("Idle")
+    root.update_idletasks()
+    listen_for_commands.counter += 1
+
+if __name__=="__main__":
+    #if not admin.isUserAdmin():
+    #    admin.runAsAdmin()
+    command_recorder = CommandRecorder()
+
+    root = Tk()
+    root.title("VoiceCommander")
+
+    frame = Frame(root, width=200, height=200)
+
+    state_label_text = StringVar()
+    state_label = Label(frame, textvariable=state_label_text)
+    state_label.pack()
+    state_label_text.set("Idle")
+
+    recording_button = Button(frame, text="Record command")
+    recording_button.bind("<Button-1>", listen_for_commands)
+    recording_button.pack()
+
+    frame.bind("<KeyRelease- >", listen_for_commands)
+
+    frame.pack()
+    frame.focus_set()
+    root.mainloop()
