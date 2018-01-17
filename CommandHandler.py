95acbc5fd4f9eb4aa529eec9e3513b6a7bb0f566
Jan Wilczek <janekw@vp.pl>
2018-01-17 19:14:21 +0100

    New commands implemented.

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,8 +1,12 @@
 import admin
-
 if not admin.isUserAdmin():
     admin.runAsAdmin()
+
 from pywinauto.application import Application
+from pywinauto.keyboard import SendKeys
+from pywinauto.mouse import scroll
+from CommandRecorder import CommandRecorder
+from win32api import GetCursorPos
 
 class CommandHandler:
 
@@ -22,20 +26,57 @@ class CommandHandler:
         """
 
         if command=="execute":
+            self.__execute(args)
+        elif command=="start_dictation":    # not implemented
+            dictation_recorder = CommandRecorder()
+            dictation_recorder.startRecording("dictation.wav")
+            # additional code
+        elif command=="home":
+            SendKeys('{HOME}')
+        elif command=="desktop":
+            SendKeys('{LWIN}d')
+        elif command=="up":
+            scroll(coords=GetCursorPos(), wheel_dist=10)
+        elif command=="down":
+            scroll(coords=GetCursorPos(), wheel_dist=-10)
+        elif command=="select_all":
+            SendKeys('^a')
+        elif command=="copy":
+            SendKeys('^c')
+        elif command=="paste":
+            SendKeys('^v')
+        elif command == "save":
+            SendKeys('^s')
             if args is not None:
-                # Handle the command
-                app = Application()
-                if args[0]=='chrome' or args[0]=='chrome.exe':
-                    app.start(self.chrome_path)
-                elif args[0]=='opera' or args[0]=='opera.exe':
-                    app.start(self.opera_path)
-                elif args[0]=='edge' or args[0]=='edge.exe':
-                    app.start(self.edge_path)
-                elif args[0]=='notepad':
-                    app.start(self.notepad_path)
-                elif args[0]=='explorer':
-                    app.start(self.explorer_path)
-                else:
-                    raise NotImplementedError('Given program handling not implemented!')
+                SendKeys(str(args[0]) + '{ENTER}')
+        elif command=="undo":
+            SendKeys('^z')
+        elif command=="google":     # WIP
+             app = Application(backend='uia')
+             app.start(self.chrome_path + ' --force-renderer-accessibility https://www.google.com')
+             #SendKeys("google.com{ENTER}")
+             if args is not None:
+                app_new_tab = Application(backend='uia').connect(path='chrome.exe', title_re='New Tab')
+                #app_new_tab.window().type_keys(str(args[0]) + '{ENTER}')
+                SendKeys(str(args[0]) + '{ENTER}')
+        elif command=="close":
+            SendKeys('%{F4}')
+
+    def __execute(self, args):
+        if args is not None:
+            # Handle the execute command
+            app = Application()
+            if args[0] == 'chrome' or args[0] == 'chrome.exe':
+                app.start(self.chrome_path)
+            elif args[0] == 'opera' or args[0] == 'opera.exe':
+                app.start(self.opera_path)
+            elif args[0] == 'edge' or args[0] == 'edge.exe':
+                app.start(self.edge_path)
+            elif args[0] == 'notepad':
+                app.start(self.notepad_path)
+            elif args[0] == 'explorer':
+                app.start(self.explorer_path)
             else:
-                raise Exception('Specify the program to launch!')
+                raise NotImplementedError('Given program handling not implemented!')
+        else:
+            raise Exception('Specify the program to launch!')
--- tests/command_handler_test.py
+++ tests/command_handler_test.py
@@ -1,8 +1,32 @@
 from CommandHandler import CommandHandler
 
 command_handler = CommandHandler()
+'''
 command_handler.handle('execute', 'chrome.exe')
 command_handler.handle('execute', 'edge')
-command_handler.handle('execute', 'notepad')
 command_handler.handle('execute', 'explorer')
+'''
+command_handler.handle('google', 'agh')
+command_handler.handle('down')
+command_handler.handle('down')
+command_handler.handle('home')
+command_handler.handle('down')
+command_handler.handle('down')
+command_handler.handle('up')
+command_handler.handle('select_all')
+'''
+command_handler.handle('copy')
+command_handler.handle('execute', 'notepad')
+command_handler.handle('paste')
+command_handler.handle('save', 'test')
+command_handler.handle('undo')
+command_handler.handle('close')
+command_handler.handle('desktop')
+'''
+
+
+
+
+
+
 
